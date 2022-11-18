bfda81ecdf8b0d65a869a042262a2263c03b2f6b
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-18 21:09:01 +0100

    design at create done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,10 @@ import {Link} from 'react-router-dom'
 function NavBar() {
   return (
     
-    <nav className="navbar bg-info">
-        <div className="container-fluid">
-            <Link to="/" style={{textDecoration: 'none',color: 'inherit'}}>
+    <nav className="navbar bg-info" style={{borderRadius: '10px'}}>
+        <div className="container-fluid" style={{display: 'flex', justifyContent: 'center'}}>
+            <Link to="/" style={{textDecoration: 'none',color: 'inherit' }}>
+            
             <img src={home} className="d-inline-block align-text-top" alt="..." style={{width: '60px'}}/>
             </Link>
         </div>
--- src/components/NewBeer.jsx
+++ src/components/NewBeer.jsx
@@ -6,38 +6,63 @@ import axios from "axios";
 function NewBeer() {
 
   const [name, setNewName] = useState('')
-  const [image, setNewImage] = useState('')
-  const [calories, setNewCalories] = useState(0)
-  const [servings, setNewServings] = useState(0)
+  const [tagline , setNewTagline] = useState('')
+  const [description , setNewDescription] = useState('')
+  const [first_brewed , setNewFirst_brewed] = useState('')
+  const [brewers_tips , setNewBrewers_tips] = useState('')
+  const [attenuation_level, setNewAttenuation_level] = useState(0)
+  const [contributed_by , setNewContributed_by] = useState('')
+  
 
   const handleSubmit = event =>{
         
     event.preventDefault();
-    const NewBeer = {name, image, calories, servings}
-    //console.log(NewFood)
+    const NewBeer = {name, tagline, description, first_brewed, brewers_tips, attenuation_level, contributed_by}
+    //console.log(NewBeer)
     
     setNewName('')
-    setNewImage('')
-    setNewCalories(0)
-    setNewServings(0)
+    setNewTagline('')
+    setNewDescription('')
+    setNewFirst_brewed('')
+    setNewBrewers_tips('')
+    setNewAttenuation_level(0)
+    setNewContributed_by('')
 }  
 
 
   return (
-    <div className="container" style={{marginTop: '50px'}}>
-      <NavBar/>
-      <form>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <div className="container" style={{marginTop: '50px', width: '60%'}}>
+      <NavBar />
+      <form style={{marginTop: '30px',display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '15px'}}>
+        <div className="col-sm-3">
+          <input type="text" className="form-control"  placeholder="Name" />
         </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" />
+
+        <div className="col-sm-3">
+          <input type="text" className="form-control"  placeholder="Tagline" />
+        </div>
+
+        <div className="col-sm-3">
+          <textarea type="text" className="form-control"  placeholder="Description" rows="4"/>
+        </div>
+
+        <div className="col-sm-3">
+          <input type="text" className="form-control"  placeholder="First brewed" />
+        </div>
+
+        <div className="col-sm-3">
+          <input type="text" className="form-control"  placeholder="Brewers tips" />
+        </div>
+
+        <div className="col-sm-3">
+          <input type="number" className="form-control"  placeholder="Attenuation Level" />
+        </div>
+
+        <div className="col-sm-3">
+          <input type="text" className="form-control"  placeholder="Contributed by" />
         </div>
         
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Create</button>
       </form>
     </div>
   )
